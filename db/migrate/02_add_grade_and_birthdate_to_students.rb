f311eb02099486db67c08a5f828d5bf15ce942c3
Ryna Truong <rynatruong@outlook.com>
2020-11-18 18:59:41 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddGradeAndBirthdateToStudents < ActiceRecord:: Migration[5.1]
+class AddGradeAndBirthdateToStudents < ActiveRecord:: Migration[5.1]
   
   def change
     add_coulmn :grade, :birthdate, :string
